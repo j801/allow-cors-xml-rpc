e1cd80b9596f4efc7a087899ef995d2d7986aa55
root <root@j801.com>
2013-10-18 14:45:21 +0900

    fixed Plugin URI

--- allow-cors-xml-rpc.php
+++ allow-cors-xml-rpc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Allow CORS XML-RPC
-Plugin URI: https://github.com/j801/allow_cors_xml_rpc
+Plugin URI: https://github.com/j801/allow-cors-xml-rpc
 Description: This plugin Allow CORS for XML-RPC API in WordPress 3.5+.
 Version: 0.1
 Author: YUKI OKAMOTO (HN:Justice)
